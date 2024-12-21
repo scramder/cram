38ec69e599cb61d5dc5a69e8e19c9ceb6fd7332c
Cristhian Muriel <cralmuriel@gmail.com>
2024-12-21 14:34:26 -0300

    re link scripts 	modified:   latam/latam.html 	new file:   scripts/scroll-btn.js 	modified:   styles/general.css 	deleted:    world/carousel.js 	deleted:    world/msj-rot.js 	deleted:    world/overlay.js 	deleted:    world/whatsapp.js 	modified:   world/world.html

--- world/msj-rot.js
+++
@@ -1,5 +0,0 @@
-const elements = document.querySelectorAll('.container.msj'); // Selecciona todos los elementos con las clases 'container' y 'mensaje'
-
-elements.forEach((element, index) => {
-  element.style.setProperty('--animation-delay', `${index * 3}s`); // Asigna el retraso según el índice del elemento
-});
